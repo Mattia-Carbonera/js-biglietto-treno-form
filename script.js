8515374ef838e96ccde47005ff4a1ac741e9ac54
Mattia-Carbonera <mattia.carbonera@gmail.com>
2024-10-23 15:47:30 +0200

    MILESTONE 1

--- script.js
+++ script.js
@@ -2,12 +2,40 @@
 
 const userName = document.getElementById("input-name");
 const kilometers = document.getElementById("input-chilometri");
-const userAge = document.getElementById("age-container");
+const userAgeRequest = document.getElementById("age-container");
 const buttonGenera = document.getElementById("button-genera");
+
+// ---------------------------CALCOLO PREZZO BIGLIETTO---------------------------
+function priceCounter(kilometersAdded, ageOfPassenger) {
+  // sconti da applicare
+  const totalPrice = kilometersAdded * 0.21;
+
+  const normalPrice = 0;
+  const minorDiscount = (totalPrice / 100) * 20;
+  const majorDiscount = (totalPrice / 100) * 40;
+
+  let totalPriceDiscounted = 0;
+
+  if (ageOfPassenger == 1) {
+    return (totalPriceDiscounted = totalPrice - minorDiscount);
+  } else if (ageOfPassenger == 2) {
+    return (totalPriceDiscounted = totalPrice - normalPrice);
+  } else if (ageOfPassenger == 3) {
+    return (totalPriceDiscounted = totalPrice - majorDiscount);
+  }
+}
+
+// ---------------------------RIPORTO A SCHERMO I RISULTATI---------------------------
 
 buttonGenera.addEventListener("click", () => {
   const numberOfKilometers = parseInt(kilometers.value);
-  console.log(numberOfKilometers);
+  const userAge = userAgeRequest.value;
+  //   console.log(numberOfKilometers);
+  //   console.log(userAge);
+
+  let finisPrize = priceCounter(numberOfKilometers, userAge);
+
+  console.log("Il prezzo finale è di €", finisPrize);
 });
 
 // conso)e.log(userNameEl);
